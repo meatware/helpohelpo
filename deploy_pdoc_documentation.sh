e25ae038e21edd521a280d97d6d45a6e797a8cfc
Girish <wetware@zoho.com>
2020-03-23 20:56:06 +0000

    add mkdocs theme

--- deploy_pdoc_documentation.sh
+++ deploy_pdoc_documentation.sh
@@ -2,7 +2,11 @@
 
 pdoc --html --force helpers --template-dir docs/custom_pdoc_templates/
 
+echo -e "\nCopying Pdoc html"
 cp -v html/helpers/*.html docs/
+
+echo -e "\nCopying Coverage html"
+cp -rfv htmlcov docs/
 
 git add docs/*.html
 
