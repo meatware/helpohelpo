0913b1f791a90bf70bae8ee4e46381ad18352752
Girish <wetware@zoho.com>
2020-03-23 18:40:11 +0000

    trim some fat

--- helpers/date_tools.py
+++ helpers/date_tools.py
@@ -12,7 +12,17 @@ import re
 from dateutil import tz
 from pydash import get
 
-# import pandas as pd
+LOG = logging.getLogger(__name__)
+
+
+def get_unix_ts_unit(unix_ts):
+    """Check if linux timestamp units are in seconds or ms."""
+
+    if len(unix_ts) == 13:
+        unit = "ms"
+    elif len(unix_ts) == 10:
+        unit = "s"
+    return unit
 
 
 def convert_unix_time(unix_time):
@@ -75,32 +85,11 @@ def calc_days_from_2dates_diff(prev_date):
     return ndays_back.days
 
 
-def find_nested_key(aws_response, key_path):
-    """Check if nested mixed dictionary/list object has dict key."""
-
-    return get(aws_response, key_path)
-
-
 def datetimeformat(date_str):
     """Print relative time from date_str."""
 
     mydate_time = arrow.get(date_str)
     return mydate_time.humanize()
-
-
-def find_ts_units(ts):
-    """Check units of unix time possibly?"""
-    if len(ts) == 13:
-        unit = "ms"
-    elif len(ts) == 10:
-        unit = "s"
-    return unit
-
-
-def string_to_datetime(date: str) -> datetime:
-    """Convert string into a datetime object."""
-
-    return datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
 
 
 def unixtime_to_datestring(unix_time):
@@ -126,96 +115,8 @@ def date_from_string(mydate_str):
     return datetime.strptime(mydate_str, "%Y-%m-%d").date()
 
 
-def check_ts_units(unix_ts):
-    """Check if linux timestamp units are in seconds or ms."""
+# TODO: Move to another module
+def find_nested_key(aws_response, key_path):
+    """Check if nested mixed dictionary/list object has dict key. Uses pydash get"""
 
-    if len(unix_ts) == 13:
-        unit = "ms"
-    elif len(unix_ts) == 10:
-        unit = "s"
-    return unit
-
-
-def rm_col_names(_df, exclude_list):
-    """Take dataframe column names and remove those in supplied exclude use."""
-
-    col_names = list(_df.columns.values)
-    for excl_str in exclude_list:
-        try:
-            col_names.remove(excl_str)
-        except KeyError:
-            pass
-
-    return col_names
-
-
-def drop_cols(_df, drop_list):
-    """Drop columns from dataframe supplied in drop_list."""
-
-    try:
-        _df.drop(drop_list, axis=1, inplace=True)
-    except ValueError:
-        pass
-
-    return _df
-
-
-def read_data_sim(infile):
-    """Read simulation csv file."""
-
-    sim_ts_data = pd.read_csv(infile, sep=",", header=0)
-
-    return sim_ts_data
-
-
-def slice_dataframe(_df, start, finish):
-    """Prob deprecated."""
-
-    sliced = _df[start:finish].copy()
-    slice_timestamp = unixtime_to_datetime(sliced["binned_ts"].iloc[0])
-
-    return (sliced, slice_timestamp)
-
-
-def slice_dataframe2(_df, start, finish):
-    """Slice simulation dataframe into windowed segments."""
-
-    sliced = _df[start:finish].copy()
-
-    return sliced
-
-
-def get_segment_1st_ts(unix_ts):
-    """Slice simulation dataframe into windowed segments."""
-
-    if len(str(unix_ts)) == 13:
-        formatted_ts = unix_ts // 1000
-    elif len(str(unix_ts)) == 10:
-        formatted_ts = unix_ts
-
-    first_timestamp = unixtime_to_datetime(formatted_ts)
-
-    return first_timestamp
-
-
-def delfiles_not_in_list(folder, exclude_list):
-    """Delete all files in folder aprt from those in exclude list."""
-
-    ### prepare delete list
-    del_files = os.listdir(folder)
-    # TODO: this is repeated in rm_col_names (generalise)
-    for excl_str in exclude_list:
-        try:
-            del_files.remove(excl_str)
-        except (ValueError, KeyError):
-            pass
-
-    ### delete files
-    for rmfile in del_files:
-        file_path = os.path.join(folder, rmfile)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as err:
-            print(err)
-    return
+    return get(aws_response, key_path)
--- helpers/dateframe_tools.py
+++ helpers/dateframe_tools.py
@@ -3,44 +3,13 @@
 import os
 from datetime import datetime
 import pandas as pd
-
+import logging
 from helpers.date_tools import unixtime_to_datetime
 import os
 from datetime import datetime
 import pandas as pd
 
-
-def string_to_datetime(date: str) -> datetime:
-    """Convert string into a datetime object."""
-
-    return datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
-
-
-def unixtime_to_datestring(unix_time):
-    """Convert unix timestamp into a string."""
-
-    return datetime.fromtimestamp(unix_time).strftime("%Y-%m-%d %H:%M:%S")
-
-
-# TODO: unify redundant time functions
-def time_from_string(mytime_str, tformat="%H:%M"):
-    """Converts a string into a time object."""
-    return datetime.strptime(mytime_str, tformat).time()
-
-
-def date_from_string(mydate_str):
-    """Converts a string into a date object."""
-    return datetime.strptime(mydate_str, "%Y-%m-%d").date()
-
-
-def check_ts_units(unix_ts):
-    """Check if linux timestamp units are in seconds or ms."""
-
-    if len(unix_ts) == 13:
-        unit = "ms"
-    elif len(unix_ts) == 10:
-        unit = "s"
-    return unit
+LOG = logging.getLogger(__name__)
 
 
 def rm_col_names(_df, exclude_list):
@@ -90,19 +59,6 @@ def slice_dataframe2(_df, start, finish):
     sliced = _df[start:finish].copy()
 
     return sliced
-
-
-def get_segment_1st_ts(unix_ts):
-    """Slice simulation dataframe into windowed segments."""
-
-    if len(str(unix_ts)) == 13:
-        formatted_ts = unix_ts // 1000
-    elif len(str(unix_ts)) == 10:
-        formatted_ts = unix_ts
-
-    first_timestamp = unixtime_to_datetime(formatted_ts)
-
-    return first_timestamp
 
 
 def delfiles_not_in_list(folder, exclude_list):
@@ -134,65 +90,3 @@ def set_time_as_index(_df):
     _df.index = _df["time"]
     _df.drop(["time"], axis=1, inplace=True)
     return _df
-
-
-def rm_col_names(_df, exclude_list):
-    """Take dataframe column names and remove those in supplied exclude use."""
-
-    col_names = list(_df.columns.values)
-    for excl_str in exclude_list:
-        try:
-            col_names.remove(excl_str)
-        except KeyError:
-            pass
-
-    return col_names
-
-
-def drop_cols(_df, drop_list):
-    """Drop columns from dataframe supplied in drop_list."""
-
-    try:
-        _df.drop(drop_list, axis=1, inplace=True)
-    except ValueError:
-        pass
-
-    return _df
-
-
-def read_data_sim(infile):
-    """Read simulation csv file."""
-
-    sim_ts_data = pd.read_csv(infile, sep=",", header=0)
-
-    return sim_ts_data
-
-
-def slice_dataframe(_df, start, finish):
-    """Prob deprecated."""
-
-    sliced = _df[start:finish].copy()
-    slice_timestamp = unixtime_to_datetime(sliced["binned_ts"].iloc[0])
-
-    return (sliced, slice_timestamp)
-
-
-def slice_dataframe2(_df, start, finish):
-    """Copy a df segment from df[x:y]."""
-
-    sliced = _df[start:finish].copy()
-
-    return sliced
-
-
-# def get_segment_1st_ts(unix_ts):
-#     """Slice simulation dataframe into windowed segments."""
-
-#     if len(str(unix_ts)) == 13:
-#         formatted_ts = unix_ts // 1000
-#     elif len(str(unix_ts)) == 10:
-#         formatted_ts = unix_ts
-
-#     first_timestamp = unixtime_to_datetime(formatted_ts)
-
-#     return first_timestamp
