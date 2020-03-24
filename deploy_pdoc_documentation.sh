0ab88c26ccd5c723098952ad99d4e7b7f00790d4
Girish <wetware@zoho.com>
2020-03-24 01:43:02 +0000

    switch documentation to portray

--- deploy_pdoc_documentation.sh
+++ deploy_pdoc_documentation.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-pdoc --html --force helpers --template-dir docs/custom_pdoc_templates/
+# pdoc --html --force helpers --template-dir docs/custom_pdoc_templates/
 
-echo -e "\nCopying Pdoc html"
-cp -v html/helpers/*.html docs/
+# echo -e "\nCopying Pdoc html"
+# cp -v html/helpers/*.html docs/
 
-echo -e "\nCopying Coverage html"
-cp -rfv htmlcov docs/
+# echo -e "\nCopying Coverage html"
+# cp -rfv htmlcov docs/
 
-git add docs/*.html
+# git add docs/*.html
+
+
+
+
+portray as_html  --output_dir psite_temp --overwrite True 
+cp -rf psite_temp/* docs
+
+cd docs/
+rm *.sh *.txt 
+cd -
+
+
+git add docs/*
 
 echo -e "\n Please push documentation changes to repo"
---
+++ docs/setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+
+# https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+# setup(
+#     name="helpers",  # How you named your package folder (MyLib)
+#     packages=["helpers"],  # Chose the same as "name"
+#     version="0.1",  # Start with a small number and increase it with every change you make
+#     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+#     description="???????????????",  # Give a short description about your library
+#     author="meatware",  # Type in your name
+#     author_email="your.email@domain.com",  # Type in your E-Mail
+#     url="https://github.com/meatware/helpohelpo",  # Provide either the link to your github or to your website
+#     download_url="https://github.com/meatware/helpohelpo/archive/v_01.tar.g",  # I explain this later on
+#     keywords=[
+#         "SOME",
+#         "MEANINGFULL",
+#         "KEYWORDS",
+#     ],  # Keywords that define your package best
+#     install_requires=["validators", "beautifulsoup4",],  # I get to this in a second
+#     classifiers=[
+#         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+#         "Intended Audience :: Developers",  # Define that your audience are developers
+#         "Topic :: Software Development :: Build Tools",
+#         "License :: OSI Approved :: MIT License",  # Again, pick a license
+#         "Programming Language :: Python :: 3.6",  # Specify which pyhton versions that you want to support
+#     ],
+# )
+
+# [tool.portray]
+# name = "helpohelpo"
+# version = "0.1"
+# description = "A library of python helpers"
+# authors = "meatware happy@zoho.com"
+# homepage = "https://github.com/meatware/helpohelpo"
+# modules = ["helpohelpo"]
+# license = "MIT"
+# packages = [
+#     { include = "helpers/*.py" }
+# ]
+
+# https://timothycrosley.github.io/portray/reference/portray/render/
